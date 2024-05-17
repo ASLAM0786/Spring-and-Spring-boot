a5ce9842f32b1def16bfd8a08542f66347418ac7
Aslam Ansari <aansari@quark.com>
2024-05-17 11:23:55 +0530

    Basic Todo app completed, Will work on improving look and feel

--- UdemyCoucesPractice/myfirstwebapp/src/main/java/com/aslam/springboot/myfirstwebapp/todo/TodoController.java
+++ UdemyCoucesPractice/myfirstwebapp/src/main/java/com/aslam/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,7 +1,9 @@
 package com.aslam.springboot.myfirstwebapp.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,14 +31,47 @@ public class TodoController {
 
 
     @RequestMapping(value = "add-todo", method = RequestMethod.GET)
-    public String showTodoPage() {
+    public String showTodoPage(ModelMap modelMap) {
+        String username = (String) modelMap.get("username");
+        Todo todo = new Todo(10, username, "", LocalDate.now().plusYears(1), false);
+        modelMap.put("todo", todo);
         return "todo";
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodo(@RequestParam String description, ModelMap modelMap) {
-        String username = (String) modelMap.get("username");
-        todoService.addTodo(username, description, LocalDate.now().plusMonths(7), false);
+    public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "todo";
+        }
+
+        String username = (String) model.get("name");
+        //LocalDate localDate = LocalDate.parse(todo.getTargetDate());//todo.getTargetDate();
+        todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
+        return "redirect:list-todos";
+    }
+
+    @RequestMapping("delete-todo")
+    public String deleteTodo(@RequestParam long id, ModelMap model) {
+
+        todoService.deleteBuyId(id);
+        return "redirect:list-todos";
+    }
+
+    @RequestMapping(value = "update-todo", method = RequestMethod.GET)
+    public String ShowUpdateTodo(@RequestParam long id, ModelMap model) {
+        Todo todo = todoService.FindBuyId(id);
+        model.addAttribute("todo", todo);
+        return "todo";
+    }
+
+
+    @RequestMapping(value = "update-todo", method = RequestMethod.POST)
+    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "todo";
+        }
+        todoService.updateTodo(todo);
         return "redirect:list-todos";
     }
 }
--- UdemyCoucesPractice/myfirstwebapp/src/main/java/com/aslam/springboot/myfirstwebapp/todo/TodoService.java
+++ UdemyCoucesPractice/myfirstwebapp/src/main/java/com/aslam/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,10 +1,12 @@
 package com.aslam.springboot.myfirstwebapp.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class TodoService {
@@ -28,6 +30,25 @@ public class TodoService {
 
     public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
         Todo todo = new Todo(++todoCount, username, description, targetDate, done);
+        todos.add(todo);
+    }
+
+    public void deleteBuyId(long id) {
+        //match todo id exits then delete
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+
+    public Todo FindBuyId(long id) {
+        //match todo id exits then delete
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+    }
+
+    public void updateTodo(@Valid Todo todo) {
+        deleteBuyId(todo.getId());
         todos.add(todo);
     }
 }
