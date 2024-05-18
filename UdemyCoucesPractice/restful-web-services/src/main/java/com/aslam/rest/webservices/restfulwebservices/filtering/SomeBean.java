205c2b09c6a43bebef421f21b818d2cb9f4b03ce
Aslam Ansari <aansari@quark.com>
2024-05-18 16:05:35 +0530

    dynamic filtering

--- UdemyCoucesPractice/restful-web-services/src/main/java/com/aslam/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ UdemyCoucesPractice/restful-web-services/src/main/java/com/aslam/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -4,8 +4,16 @@ package com.aslam.rest.webservices.restfulwebservices.filtering;
  * Static Filtering example using JsonIgnore for filed level and JsonIgnoreProperties for class levels
  * in class level we can either single value or arrays of values to include more filed
  */
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+/**
+ * For Dynamic Filtering use JsonFilter
+ */
+
 //@JsonIgnoreProperties("field1")
 //@JsonIgnoreProperties({"field1","field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
 
